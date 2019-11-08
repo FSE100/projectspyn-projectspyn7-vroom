fc66865a2c5bd480c0731b27ffd7378d1808f22c
Louis Jaeckle <49696643+louisjaeckle@users.noreply.github.com>
2019-11-07 20:11:42 -0700

    Update FindPassenger.m

--- FindPassenger.m
+++ FindPassenger.m
@@ -21,38 +21,38 @@ while move
 		brick.StopAllMotors(‘Brake’);
 		brick.MoveMotorAngleRel(‘D’,1000,-270,’Brake’);
 		brick.MoveMotorAngleRel(‘B’,1000,270,’Brake’);
-        brick.WaitForMotor('B');
-        brick.WaitForMotor('D');
+        	brick.WaitForMotor('B');
+        	brick.WaitForMotor('D');
 		brick.StopAllMotors(‘Brake’);
-        initialDist = brick.UltrasonicDist(1);
+        	initialDist = brick.UltrasonicDist(1);
 	elseif distance>30
 		brick.StopAllMotors(‘Brake’);
 		%check if gap in wall or end of wall
 		brick.MoveMotorAngleRel(‘D’,1000,100,’Brake’);%adjust values
 		brick.MoveMotorAngleRel(‘B’,1000,100,’Brake’);
-        brick.WaitForMotor('B');
-        brick.WaitForMotor('D');
+        	brick.WaitForMotor('B');
+        	brick.WaitForMotor('D');
 		if distance>30
 			%end of wall, turn left
 			brick.MoveMotorAngleRel(‘D’,1000,500,’Brake’);%adjust values
 			brick.MoveMotorAngleRel(‘B’,1000,100,’Brake’);
-            brick.WaitForMotor('B');
-            brick.WaitForMotor('D');
+            		brick.WaitForMotor('B');
+            		brick.WaitForMotor('D');
 			break;
 
-	elseif distance<initialDist-5
+	elseif distance<initialDist-10
 		brick.StopAllMotors(‘Brake’);
 		%turn right a little
 		brick.MoveMotorAngleRel(‘B’,1000,80,’Brake’);%adjust adjust value
-        brick.WaitForMotor('B');
+        	brick.WaitForMotor('B');
 		brick.MoveMotor(‘B’,100);
 		brick.MoveMotor(‘D’,100);
 		pause(0.5);
-	elseif distance>initialDist+5
+	elseif distance>initialDist+10
 		brick.StopAllMotors(‘Brake’);
 		%turn left a little
 		brick.MoveMotorAngleRel(‘D’,1000,80,’Brake’);%adjust value
-        brick.WaitForMotor('D');
+        	brick.WaitForMotor('D');
 		brick.MoveMotor(‘B’,100);
 		brick.MoveMotor(‘D’,100);
 		pause(0.5);
