52407926416a8e4b33e4547f946fcb7d409f5037
Louis Jaeckle <49696643+louisjaeckle@users.noreply.github.com>
2019-11-07 20:05:18 -0700

    Update FindPassenger.m

--- FindPassenger.m
+++ FindPassenger.m
@@ -1,39 +1,62 @@
 move = true;
-brick.SetColorMode(1, 2);  % Set Color Sensor connected to Port 1 to Color Code Mode
+brick.SetColorMode(1,2);
+initialDist = brick.UltrasonicDist(1);
 
 while move
-    crash = brick.TouchPressed(3) == 1 || brick.TouchPressed(4) == 1;
-    %display(crash);
-    color = brick.ColorCode(2);
-    distance = brick.UltrasonicDist(1);
-    
-    if color == 3
-        brick.StopAllMotors('Brake');
-        break;
-    end
-    
-    if crash == 1
-        %turn right
-        brick.StopAllMotors('Brake');
-        brick.MoveMotor('B', -50);
-        brick.MoveMotor('D', -50);
-        pause(0.5);
-        brick.StopAllMotors('Brake');
-        brick.MoveMotorAngleRel('D', 1000, -270, 'Brake');
-        brick.MoveMotorAngleRel('B', 1000, 270, 'Brake');
-        pause(0.5);
-        brick.StopAllMotors('Brake');
-    elseif distance < 10
-        %veer right
-        display("this goes right");
-        brick.MoveMotor('B', 60);
-        brick.MoveMotor('D', 50);
-    else
-        %veer left
-        display("this goes left");
-        brick.MoveMotor('B', 50);
-        brick.MoveMotor('D', 55);
-    end
-    
-end
+	crash = brick.TouchPressed(3) == 1 || brick.TouchPressed(4) == 1;
+	color = brick.ColorCode(2);
+	distance = brick.UltrasonicDist(1);
 
+	if color == 3;
+		brick.StopAllMotors(‘Brake’);
+		break;
+	end
+
+	if crash == 1
+	%turn right
+		brick.StopAllMotors(‘Brake’);
+		brick.MoveMotor(‘B’,-50);
+		brick.MoveMotor(‘D’,-50);
+		pause(0.5);
+		brick.StopAllMotors(‘Brake’);
+		brick.MoveMotorAngleRel(‘D’,1000,-270,’Brake’);
+		brick.MoveMotorAngleRel(‘B’,1000,270,’Brake’);
+        brick.WaitForMotor('B');
+        brick.WaitForMotor('D');
+		brick.StopAllMotors(‘Brake’);
+        initialDist = brick.UltrasonicDist(1);
+	elseif distance>30
+		brick.StopAllMotors(‘Brake’);
+		%check if gap in wall or end of wall
+		brick.MoveMotorAngleRel(‘D’,1000,100,’Brake’);%adjust values
+		brick.MoveMotorAngleRel(‘B’,1000,100,’Brake’);
+        brick.WaitForMotor('B');
+        brick.WaitForMotor('D');
+		if distance>30
+			%end of wall, turn left
+			brick.MoveMotorAngleRel(‘D’,1000,500,’Brake’);%adjust values
+			brick.MoveMotorAngleRel(‘B’,1000,100,’Brake’);
+            brick.WaitForMotor('B');
+            brick.WaitForMotor('D');
+			break;
+
+	elseif distance<initialDist-5
+		brick.StopAllMotors(‘Brake’);
+		%turn right a little
+		brick.MoveMotorAngleRel(‘B’,1000,80,’Brake’);%adjust adjust value
+        brick.WaitForMotor('B');
+		brick.MoveMotor(‘B’,100);
+		brick.MoveMotor(‘D’,100);
+		pause(0.5);
+	elseif distance>initialDist+5
+		brick.StopAllMotors(‘Brake’);
+		%turn left a little
+		brick.MoveMotorAngleRel(‘D’,1000,80,’Brake’);%adjust value
+        brick.WaitForMotor('D');
+		brick.MoveMotor(‘B’,100);
+		brick.MoveMotor(‘D’,100);
+		pause(0.5);
+	else
+		brick.MoveMotor(‘B’,100);
+		brick.MoveMotor(‘D’,100);
+		
