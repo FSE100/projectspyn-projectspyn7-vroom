4e0639cffbca6dffe1dd3932e75fa0fef194fbfb
Oliver Ratnam <oratnam@asu.edu>
2019-11-08 17:19:47 -0700

    Refined the manual control to pick up passenger

--- ManualControl.m
+++ ManualControl.m
@@ -3,39 +3,39 @@ InitKeyboard();
 
 while 1
     pause(0.1);
-    switch key
+     switch key
         case 'uparrow'
             disp('Up Arrow Pressed!');
-            brick.MoveMotor('A', 100);
-            brick.MoveMotor('D', 100);
-            
+            brick.MoveMotor('B', 25);
+            brick.MoveMotor('D', 25);
+
         case 'downarrow'
             disp('Down Arrow Pressed!');
-            brick.MoveMotor('A', -50);
-            brick.MoveMotor('D', -50);
-            
+            brick.MoveMotor('B', -25);
+            brick.MoveMotor('D', -25);
+
         case 'leftarrow'
             disp('Left Arrow Pressed!');
-            brick.MoveMotor('D', 50);
-        
+            brick.MoveMotor('D', 25);
+
         case 'rightarrow'
             disp('Right Arrow Pressed!');
-            brick.MoveMotor('A', 50);
-            
+            brick.MoveMotor('B', 25);
+
         case '1'
             disp('Motor Raised!');
-            brick.MoveMotorAngleRel('C', 5, -45, 'Brake');
-            pause(0.5);
-        
+            brick.MoveMotorAngleRel('C', 2, -50, 'Coast');
+            pause(1.5);
+
         case '2'
             disp('Motor Lowered!');
-            brick.MoveMotorAngleRel('C', 5, 25, 'Brake');
-            pause(0.5);
-            
+            brick.MoveMotorAngleRel('C', 2, 50, 'Coast');
+            pause(1.5);
+
         case 0
             disp('No Key Pressed');
             brick.StopAllMotors('Brake');
-        
+
         case 'q'
             break;
     end
