bdb3a641661b2f735c55a0e9d299d37e66abe16d
Ninad Kulkarni <ninadk03@gmaik.com>
2019-11-15 17:29:51 -0700

    Worked on perfecting the color sensor. Needs more work. Change sensor hardware height.

--- Automated_MASTER.m
+++ Automated_MASTER.m
@@ -5,37 +5,34 @@
 %Port 3 = Left Crash Sensor
 %Port 4 = Right Crash Sensor
 
-if brick == null
-	connectToEV3;
-end
-
 brick.SetColorMode(2, 2);  % Set Color Sensor connected to Port 2 to Color Code Mode
 
-pickupColor = colorAssigner(lower(input("Enter pickup color: ")));
-dropOffColor = colorAssigner(lower(input("Enter drop-off color: ")));
-
-goToDestination(pickupColor);
+goToDestination(3);
 manualControl();
-goToDestination(dropOffColor);
+goToDestination(2);
 manualControl();
 
-function goToDestination(pickupColor)
+function goToDestination(colorDestination)
+    global brick
 	move = true;
 	
 	while move
 		crash = brick.TouchPressed(3) == 1 || brick.TouchPressed(4) == 1;
 		%display(crash);
 		color = brick.ColorCode(2);
+        disp(color);
 		distance = brick.UltrasonicDist(1);
 		
-		if color == pickupColor
+		if color == colorDestination
 			%At pickup or drop-off color completely stop the car and break the loop
 			brick.StopAllMotors('Brake');
 			break;
 		elseif color == 5
-			%At red pause the car for 2 seconds
+			%At red pause the car for 4 seconds
 			brick.StopAllMotors('Brake');
-			pause(2);
+			pause(4);
+            brick.MoveMotor('BD', 50);
+            pause(0.5);
 		end
 		
 		if crash == 1
@@ -49,25 +46,26 @@ function goToDestination(pickupColor)
 			brick.MoveMotorAngleRel('B', 100, 180, 'Brake');
 			pause(0.5);
 			brick.StopAllMotors('Brake');
-		elseif distance < 15.00
+		elseif distance < 20.00
 			%veer right
-			brick.MoveMotor('B', 60);
-			brick.MoveMotor('D', 50);
+			brick.MoveMotor('B', 100);
+			brick.MoveMotor('D', 90);
 		elseif distance > 25.00
 			%veer extreme left
-			display("this goes LOT left");
+			%display("this goes LOT left");
 			brick.MoveMotor('B', 50);
 			brick.MoveMotor('D', 70);
 		else
 			%veer left
-			display("this goes left");
-			brick.MoveMotor('B', 50);
-			brick.MoveMotor('D', 60);
+			%display("this goes left");
+			brick.MoveMotor('B', 90);
+			brick.MoveMotor('D', 100);
 		end
 	end
 end
 
 function manualControl
+    global brick
 	global key
 	InitKeyboard();
 
@@ -80,30 +78,30 @@ function manualControl
 				brick.MoveMotor('D', 25);
 
 			case 'downarrow'
-				disp('Down Arrow Pressed!');
+				%disp('Down Arrow Pressed!');
 				brick.MoveMotor('B', -25);
 				brick.MoveMotor('D', -25);
 
 			case 'leftarrow'
-				disp('Left Arrow Pressed!');
+				%disp('Left Arrow Pressed!');
 				brick.MoveMotor('D', 25);
 
 			case 'rightarrow'
-				disp('Right Arrow Pressed!');
+				%disp('Right Arrow Pressed!');
 				brick.MoveMotor('B', 25);
 
 			case '1'
-				disp('Motor Raised!');
+				%disp('Motor Raised!');
 				brick.MoveMotorAngleRel('C', 2, -50, 'Coast');
 				pause(1.5);
 
 			case '2'
-				disp('Motor Lowered!');
+				%disp('Motor Lowered!');
 				brick.MoveMotorAngleRel('C', 2, 50, 'Coast');
 				pause(1.5);
 
 			case 0
-				disp('No Key Pressed');
+				%disp('No Key Pressed');
 				brick.StopAllMotors('Brake');
 
 			case 'q'
@@ -112,28 +110,4 @@ function manualControl
 	end
 
 	CloseKeyboard();
-end
-
-function color = colorAssigner(inputColor)
-	if inputColor == 'black'
-		color = 1
-	elseif inputColor == 'blue'
-		color = 2
-	elseif inputColor == 'green'
-		color = 3
-	elseif inputColor == 'yellow'
-		color = 4
-	elseif inputColor == 'red'
-		color = 5
-	elseif inputColor == 'white'
-		color = 6
-	else
-		color = 0
-	end
-end
-
-function connectToEV3
-	brick = ConnectBrick('BEANS');
-	brick.playTone(50, 800, 500);
-	brick.GetBattVoltage();
 end
